4cf0780654729390af0358f21920f58071d16e01
Sudin Manandhar <manandharsudin@gmail.com>
2024-06-28 22:02:43 +0545

    Completed category section

--- template-parts/home/category.php
+++ template-parts/home/category.php
@@ -2,111 +2,76 @@
 /**
  * Category Section
 */
-?>
 
-<section id="mycategory" class="category lg:grid lg:grid-cols-4">
-    <div class="mx-1 lg:mx-0 categorylist bg-secondarylight pt-24 cursor-pointer overflow-hidden">
-        <div class="px-4 flex flex-col items-center relative z-20" data-aos="fade-down" data-aos-delay="50" data-aos-duration="1000">
-            <h5 class="text-primary font-semibold">75 days</h5>
-            <h3 class="text-[30px] leading[120%] text-neutral-1000 font-semibold mb-4">Everest</h5>
-            <div class="flex flex-col justify-center shortinfo space-y-4 lg:opacity-0 lg:visibility-hidden transition delay-150 ease-linear duration-150">
-                <div class="flex justify-between space-x-16">
-                    <div class="flex items-center space-x-1">
-                        <span><i class="text-base md:text-lg text-neutral-800 icon-chart"></i></span>
-                        <span class="text-base md:text-lg text-neutral-800 font-semibold">Hard</span>
+$categories = get_sub_field( 'categories' );
+
+if( $categories ){ ?>
+    <section id="mycategory" class="category lg:grid lg:grid-cols-4">
+        <?php
+            foreach( $categories as $category ){ 
+                $image       = $category['image'];
+                $hover_image = $category['hover_image'];
+                $title       = $category['title'];
+                $subtitle    = $category['subtitle'];
+                $difficulty  = $category['difficulty'];
+                $height      = $category['height'];
+                $link        = $category['link']; ?>
+                <div class="mx-1 lg:mx-0 categorylist bg-secondarylight pt-24 cursor-pointer overflow-hidden">
+                    <div class="px-4 flex flex-col items-center relative z-20" data-aos="fade-down" data-aos-delay="50" data-aos-duration="1000">
+                        <?php 
+                            if( $subtitle ) echo '<h5 class="text-primary font-semibold">' . esc_html( $subtitle ) . '</h5>';
+                            if( $title ) echo '<h3 class="text-[30px] leading[120%] text-neutral-1000 font-semibold mb-4">' . esc_html( $title ) . '</h5>';
+
+                            if( $difficulty || $height ){ ?>
+                                <div class="flex flex-col justify-center shortinfo space-y-4 lg:opacity-0 lg:visibility-hidden transition delay-150 ease-linear duration-150">
+                                    <div class="flex justify-between space-x-16">
+                                        <?php
+                                            if( $difficulty ){ ?>
+                                                <div class="flex items-center space-x-1">
+                                                    <span><i class="text-base md:text-lg text-neutral-800 icon-chart"></i></span>
+                                                    <span class="text-base md:text-lg text-neutral-800 font-semibold"><?php echo esc_html( $difficulty ); ?></span>
+                                                </div>
+                                                <?php
+                                            }
+
+                                            if( $height ){ ?>
+                                                <div class="flex items-center space-x-1">
+                                                    <span><i class="text-base md:text-lg text-neutral-800 icon-mountain"></i></span>
+                                                    <span class="text-base md:text-lg text-neutral-800 font-semibold"><?php echo esc_html( $height ); ?></span>
+                                                </div>
+                                                <?php
+                                            }
+                                        ?>
+                                    </div>
+                                    <?php 
+                                        if( $link ){ ?>
+                                            <a href="<?php echo esc_url( $link['url'] ); ?>" target="<?php echo esc_attr( $link['target'] ); ?>" class="flex items-center space-x-1 text-sm font-medium self-center hover:text-primary">
+                                                <span><?php echo esc_html( $link['title'] ); ?></span> 
+                                                <span><i class="icon-chevrons-right text-xl"></i></span>
+                                            </a>
+                                            <?php
+                                        }
+                                    ?>                                    
+                                </div>
+                                <?php
+                            }
+                        ?>
                     </div>
-                    <div class="flex items-center space-x-1">
-                        <span><i class="text-base md:text-lg text-neutral-800 icon-mountain"></i></span>
-                        <span class="text-base md:text-lg text-neutral-800 font-semibold">6,856m</span>
-                    </div>
+                    <?php
+                        if( $image || $hover_image ){ ?>
+                            <div class="imagebox relative">
+                                <?php
+                                    echo wp_get_attachment_image( $image['ID'], 'full', false, array( 'class' => 'mountain w-full transition-all duration-150 ease-linear aspect-[2/4]' ) );
+                                    echo wp_get_attachment_image( $hover_image['ID'], 'full', false, array( 'class' => 'cloud w-full absolute bottom-0 mix-blend-lighten transition-all duration-150 ease-linear' ) );
+                                ?>
+                            </div>
+                            <?php
+                        }
+                    ?>
                 </div>
-                <a href="#" class="flex items-center space-x-1 text-sm font-medium self-center hover:text-primary">
-                    <span>View Detail</span> 
-                    <span><i class="icon-chevrons-right text-xl"></i></span>
-                </a>
-            </div>
-        </div>
-        <div class="imagebox relative">
-            <img src="<?php echo trailingslashit( get_template_directory_uri() );?>assets/images/everest.png" alt="Everest" class="mountain w-full transition-all duration-150 ease-linear aspect-[2/4]"/>
-            <img src="<?php echo trailingslashit( get_template_directory_uri() );?>assets/images/cloud.png" alt="Cloud" class="cloud w-full absolute bottom-0 mix-blend-lighten transition-all duration-150 ease-linear"/>
-        </div>
-    </div>
-    <div class="mx-1 lg:mx-0 categorylist bg-secondarylight pt-24 cursor-pointer overflow-hidden">
-        <div class="px-4 flex flex-col items-center relative z-20" data-aos="fade-down" data-aos-delay="50" data-aos-duration="1000">
-            <h5 class="text-primary font-semibold">60 days</h5>
-            <h3 class="text-[30px] leading[120%] text-neutral-1000 font-semibold mb-4">MT. Makalu</h5>
-            <div class="flex flex-col justify-center shortinfo space-y-4 lg:opacity-0 lg:visibility-hidden transition delay-150 ease-linear duration-150">
-                <div class="flex justify-between space-x-16">
-                    <div class="flex items-center space-x-1">
-                        <span><i class="text-base md:text-lg text-neutral-800 icon-chart"></i></span>
-                        <span class="text-base md:text-lg text-neutral-800 font-semibold">Hard</span>
-                    </div>
-                    <div class="flex items-center space-x-1">
-                        <span><i class="text-base md:text-lg text-neutral-800 icon-mountain"></i></span>
-                        <span class="text-base md:text-lg text-neutral-800 font-semibold">6,856m</span>
-                    </div>
-                </div>
-                <a href="#" class="flex items-center space-x-1 text-sm font-medium self-center hover:text-primary">
-                    <span>View Detail</span>
-                    <span><i class="icon-chevrons-right text-xl"></i></span>
-                </a>
-            </div>
-        </div>
-        <div class="imagebox relative">
-            <img src="<?php echo trailingslashit( get_template_directory_uri() );?>assets/images/makalu.png" alt="Mt Makalu" class="mountain w-full transition-all duration-150 ease-linear aspect-[2/4]"/>
-            <img src="<?php echo trailingslashit( get_template_directory_uri() );?>assets/images/cloud2.png" alt="Cloud" class="cloud w-full absolute bottom-0 mix-blend-lighten transition-all duration-150 ease-linear"/>
-        </div>
-    </div>
-    <div class="mx-1 lg:mx-0 categorylist bg-secondarylight pt-24 cursor-pointer overflow-hidden">
-        <div class="px-4 flex flex-col items-center relative z-20" data-aos="fade-down" data-aos-delay="50" data-aos-duration="1000">
-            <h5 class="text-primary font-semibold">36 days</h5>
-            <h3 class="text-[30px] leading[120%] text-neutral-1000 font-semibold mb-4">MT. Pumori</h5>
-            <div class="flex flex-col justify-center shortinfo space-y-4 lg:opacity-0 lg:visibility-hidden transition delay-150 ease-linear duration-150">
-                <div class="flex justify-between space-x-16">
-                    <div class="flex items-center space-x-1">
-                        <span><i class="text-base md:text-lg text-neutral-800 icon-chart"></i></span>
-                        <span class="text-base md:text-lg text-neutral-800 font-semibold">Hard</span>
-                    </div>
-                    <div class="flex items-center space-x-1">
-                        <span><i class="text-base md:text-lg text-neutral-800 icon-mountain"></i></span>
-                        <span class="text-base md:text-lg text-neutral-800 font-semibold">6,856m</span>
-                    </div>
-                </div>
-                <a href="#" class="flex items-center space-x-1 text-sm font-medium self-center hover:text-primary">
-                    <span>View Detail</span>
-                    <span><i class="icon-chevrons-right text-xl"></i></span>
-                </a>
-            </div>
-        </div>
-        <div class="imagebox relative">
-            <img src="<?php echo trailingslashit( get_template_directory_uri() );?>assets/images/pumori.png" alt="MT. Pumori" class="mountain w-full transition-all duration-150 ease-linear aspect-[2/4]"/>
-            <img src="<?php echo trailingslashit( get_template_directory_uri() );?>assets/images/cloud3.png" alt="Cloud" class="cloud w-full absolute bottom-0 mix-blend-lighten transition-all duration-150 ease-linear"/>
-        </div>
-    </div>
-    <div class="mx-1 lg:mx-0 categorylist bg-secondarylight pt-24 cursor-pointer overflow-hidden">
-        <div class="px-4 flex flex-col items-center relative z-20" data-aos="fade-down" data-aos-delay="50" data-aos-duration="1000">
-            <h5 class="text-primary font-semibold">31 days</h5>
-            <h3 class="text-[30px] leading[120%] text-neutral-1000 font-semibold mb-4">MT. Manaslu</h5>
-            <div class="flex flex-col justify-center shortinfo space-y-4 lg:opacity-0 lg:visibility-hidden transition delay-150 ease-linear duration-150">
-                <div class="flex justify-between space-x-16">
-                    <div class="flex items-center space-x-1">
-                        <span><i class="text-base md:text-lg text-neutral-800 icon-chart"></i></span>
-                        <span class="text-base md:text-lg text-neutral-800 font-semibold">Hard</span>
-                    </div>
-                    <div class="flex items-center space-x-1">
-                        <span><i class="text-base md:text-lg text-neutral-800 icon-mountain"></i></span>
-                        <span class="text-base md:text-lg text-neutral-800 font-semibold">6,856m</span>
-                    </div>
-                </div>
-                <a href="#" class="flex items-center space-x-1 text-sm font-medium self-center hover:text-primary">
-                    <span>View Detail</span>
-                    <span><i class="icon-chevrons-right text-xl"></i></span>
-                </a>
-            </div>
-        </div>
-        <div class="imagebox relative">
-            <img src="<?php echo trailingslashit( get_template_directory_uri() );?>assets/images/manaslu.png" alt="MT. Manaslu" class="mountain w-full transition-all duration-150 ease-linear aspect-[2/4]"/>
-            <img src="<?php echo trailingslashit( get_template_directory_uri() );?>assets/images/cloud4.png" alt="Cloud" class="cloud w-full absolute bottom-0 mix-blend-lighten transition-all duration-150 ease-linear"/>
-        </div>
-    </div>
-</section>
+                <?php                
+            }
+        ?>
+    </section>
+    <?php
+}
