a7eb242a45fa787611101abc240024c3c889d3ea
Sudin Manandhar <manandharsudin@gmail.com>
2024-06-29 09:20:56 +0545

    completed popular trek section

--- template-parts/home/popular.php
+++ template-parts/home/popular.php
@@ -2,164 +2,101 @@
 /**
  * Popular Section
 */
-?>
 
-<section class="popularTrek py-12 md:py-16 lg:py-20">
-    <div class="container">
-        <div class="sectiontitle" data-aos="fade-down" data-aos-delay="50" data-aos-duration="1000">
-            <h2 class="text-netrual-100 text-3xl md:text-4xl lg:text-[40px] lg:leading-[120%] font-bold mb-4 md:mb-8 lg:mb-14 max-w-[628px]">Explore Our Most Popular Treks and Expeditions</h2>
+$title = get_sub_field( 'title' );
+$trips = get_sub_field( 'trips' );
+
+if( $trips ){ ?>
+    <section class="popularTrek py-12 md:py-16 lg:py-20">
+        <div class="container">
+            <?php
+                if( $title ){ ?>
+                    <div class="sectiontitle" data-aos="fade-down" data-aos-delay="50" data-aos-duration="1000">
+                        <h2 class="text-netrual-100 text-3xl md:text-4xl lg:text-[40px] lg:leading-[120%] font-bold mb-4 md:mb-8 lg:mb-14 max-w-[628px]"><?php echo esc_html( $title ); ?></h2>
+                    </div>
+                    <?php
+                }                
+            ?>
+            
+            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
+                <?php
+                    $i = 50;
+                    foreach( $trips as $trip ){ 
+                        $image      = $trip['image'];
+                        $date       = $trip['date'];
+                        $title      = $trip['title'];
+                        $difficulty = $trip['difficulty'];
+                        $days       = $trip['days'];
+                        $height     = $trip['height'];
+                        $link       = $trip['link']; ?>
+                        <a href="<?php echo esc_url( $link['url'] )?>" target="<?php echo esc_attr( $link['target'] ); ?>" class="fadedownanimate" data-aos="fade-down" data-aos-delay="<?php echo absint( $i ); ?>" data-aos-duration="1000">
+                            <?php
+                                if( $image ){ ?>
+                                    <div class="relative overflow-hidden">
+                                        <?php 
+                                            echo wp_get_attachment_image( $image['ID'], 'full', false, array( 'class' => 'transition ease-linear duration-150 hover:scale-110' ) );
+                                            
+                                            if( $date ){ ?>
+                                                <div class="absolute top-0 right-0">
+                                                    <div class="date flex bg-primary items-center p-1 md:p-2 lg:p-3 space-x-2 md:space-x-3">
+                                                        <span><i class="text-white text-xl md:text-2xl icon-CalendarDots"></i></span>
+                                                        <span class="text-white font-semibold text-base"><?php echo esc_html( $date ); ?></span>
+                                                    </div>
+                                                </div>
+                                                <?php
+                                            }
+                                        ?>
+                                    </div>
+                                    <?php      
+                                }
+
+                                if( $title || $difficulty || $days || $height ){ ?>                                
+                                    <div class="trekdescription py-4">
+                                        <?php
+                                            if( $title ) echo '<h3 class="text-netrual-100 text-xl md:text-2xl font-bold mb-3 transition ease-linear delay-150 hover:text-primary">' . esc_html( $title ) . '</h3>';
+
+                                            if( $difficulty || $days || $height ){ ?>                                        
+                                                <div class="flex items-center space-x-6">
+                                                    <?php 
+                                                        if( $difficulty ){ ?>
+                                                            <div class="flex items-center space-x-1">
+                                                                <span><i class="text-base md:text-lg text-neutral-800 icon-chart"></i></span>
+                                                                <span class="text-base md:text-lg text-neutral-800 font-semibold"><?php echo esc_html( $difficulty ); ?></span>
+                                                            </div>
+                                                            <?php
+                                                        }
+
+                                                        if( $days ){ ?>
+                                                            <div class="flex items-center space-x-1">
+                                                                <span><i class="text-base md:text-lg text-neutral-800 icon-clock"></i></span>
+                                                                <span class="text-base md:text-lg text-neutral-800 font-semibold"><?php echo esc_html( $days ); ?></span>
+                                                            </div>
+                                                            <?php
+                                                        }
+
+                                                        if( $height ){ ?>
+                                                            <div class="flex items-center space-x-1">
+                                                                <span><i class="text-base md:text-lg text-neutral-800 icon-mountain"></i></span>
+                                                                <span class="text-base md:text-lg text-neutral-800 font-semibold"><?php echo esc_html( $height ); ?></span>
+                                                            </div>
+                                                            <?php
+                                                        }
+                                                    ?>
+                                                </div>
+                                                <?php
+                                            }
+                                        ?>
+                                    </div>
+                                    <?php
+                                }
+                            ?>
+                        </a>
+                        <?php
+                        $i = $i + 100;
+                    }
+                ?>
+            </div>
         </div>
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
-            <a href="detailpage.html" class="fadedownanimate" data-aos="fade-down" data-aos-delay="50" data-aos-duration="1000">
-                <div class="relative overflow-hidden">
-                    <img src="<?php echo trailingslashit( get_template_directory_uri() );?>assets/images/trekking/trek1.jpg" alt="Trekking Photo" class="transition ease-linear duration-150 hover:scale-110"/>
-                    <div class="absolute top-0 right-0">
-                        <div class="date flex bg-primary items-center p-1 md:p-2 lg:p-3 space-x-2 md:space-x-3">
-                            <span><i class="text-white text-xl md:text-2xl icon-CalendarDots"></i></span>
-                            <span class="text-white font-semibold text-base">April-June</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="trekdescription py-4">
-                    <h3 class="text-netrual-100 text-xl md:text-2xl font-bold mb-3 transition ease-linear delay-150 hover:text-primary">Ama Dablam Expediton</h3>
-                    <div class="flex items-center space-x-6">
-                        <div class="flex items-center space-x-1">
-                            <span><i class="text-base md:text-lg text-neutral-800 icon-chart"></i></span>
-                            <span class="text-base md:text-lg text-neutral-800 font-semibold">Hard</span>
-                        </div>
-                        <div class="flex items-center space-x-1">
-                            <span><i class="text-base md:text-lg text-neutral-800 icon-clock"></i></span>
-                            <span class="text-base md:text-lg text-neutral-800 font-semibold">65 days</span>
-                        </div>
-                        <div class="flex items-center space-x-1">
-                            <span><i class="text-base md:text-lg text-neutral-800 icon-mountain"></i></span>
-                            <span class="text-base md:text-lg text-neutral-800 font-semibold">6,856m</span>
-                        </div>
-                    </div>
-                </div>
-            </a>
-            <a href="detailpage.html" class="fadedownanimate" data-aos="fade-down" data-aos-delay="150" data-aos-duration="1000">
-                <div class="relative overflow-hidden">
-                    <img src="<?php echo trailingslashit( get_template_directory_uri() );?>assets/images/trekking/trek2.jpg" alt="Trekking Photo" class="transition ease-linear duration-150 hover:scale-110"/>                
-                </div>
-                <div class="trekdescription py-4">
-                    <h3 class="text-netrual-100 text-xl md:text-2xl font-bold mb-3 transition ease-linear delay-150 hover:text-primary">Himlung Himal Expedition</h3>
-                    <div class="flex items-center space-x-6">
-                        <div class="flex items-center space-x-1">
-                            <span><i class="text-base md:text-lg text-neutral-800 icon-chart"></i></span>
-                            <span class="text-base md:text-lg text-neutral-800 font-semibold">Easy</span>
-                        </div>
-                        <div class="flex items-center space-x-1">
-                            <span><i class="text-base md:text-lg text-neutral-800 icon-clock"></i></span>
-                            <span class="text-base md:text-lg text-neutral-800 font-semibold">26 days</span>
-                        </div>
-                        <div class="flex items-center space-x-1">
-                            <span><i class="text-base md:text-lg text-neutral-800 icon-mountain"></i></span>
-                            <span class="text-base md:text-lg text-neutral-800 font-semibold">7,216m</span>
-                        </div>
-                    </div>
-                </div>
-            </a>
-            <a href="detailpage.html" class="fadedownanimate" data-aos="fade-down" data-aos-delay="250" data-aos-duration="1000">
-                <div class="relative overflow-hidden">
-                    <img src="<?php echo trailingslashit( get_template_directory_uri() );?>assets/images/trekking/trek3.jpg" alt="Trekking Photo" class="transition ease-linear duration-150 hover:scale-110"/>                    
-                    <div class="absolute top-0 right-0">
-                        <div class="date flex bg-primary items-center p-1 md:p-2 lg:p-3 space-x-2 md:space-x-3">
-                            <span><i class="text-white text-xl md:text-2xl icon-CalendarDots"></i></span>
-                            <span class="text-white font-semibold text-base">April-June</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="trekdescription py-4">
-                    <h3 class="text-netrual-100 text-xl md:text-2xl font-bold mb-3 transition ease-linear delay-150 hover:text-primary">Mera Peak Climbing</h3>
-                    <div class="flex items-center space-x-6">
-                        <div class="flex items-center space-x-1">
-                            <span><i class="text-base md:text-lg text-neutral-800 icon-chart"></i></span>
-                            <span class="text-base md:text-lg text-neutral-800 font-semibold">Easy</span>
-                        </div>
-                        <div class="flex items-center space-x-1">
-                            <span><i class="text-base md:text-lg text-neutral-800 icon-clock"></i></span>
-                            <span class="text-base md:text-lg text-neutral-800 font-semibold">26 days</span>
-                        </div>
-                        <div class="flex items-center space-x-1">
-                            <span><i class="text-base md:text-lg text-neutral-800 icon-mountain"></i></span>
-                            <span class="text-base md:text-lg text-neutral-800 font-semibold">7,216m</span>
-                        </div>
-                    </div>
-                </div>
-            </a>
-            <a href="detailpage.html" class="fadedownanimate" data-aos="fade-down" data-aos-delay="350" data-aos-duration="1000">
-                <div class="relative overflow-hidden">
-                    <img src="<?php echo trailingslashit( get_template_directory_uri() );?>assets/images/trekking/trek4.jpg" alt="Trekking Photo" class="transition ease-linear duration-150 hover:scale-110"/>                    
-                </div>
-                <div class="trekdescription py-4">
-                    <h3 class="text-netrual-100 text-xl md:text-2xl font-bold mb-3 transition ease-linear delay-150 hover:text-primary">Exploring Annapurna</h3>
-                    <div class="flex items-center space-x-6">
-                        <div class="flex items-center space-x-1">
-                            <span><i class="text-base md:text-lg text-neutral-800 icon-chart"></i></span>
-                            <span class="text-base md:text-lg text-neutral-800 font-semibold">Easy</span>
-                        </div>
-                        <div class="flex items-center space-x-1">
-                            <span><i class="text-base md:text-lg text-neutral-800 icon-clock"></i></span>
-                            <span class="text-base md:text-lg text-neutral-800 font-semibold">26 days</span>
-                        </div>
-                        <div class="flex items-center space-x-1">
-                            <span><i class="text-base md:text-lg text-neutral-800 icon-mountain"></i></span>
-                            <span class="text-base md:text-lg text-neutral-800 font-semibold">3,216m</span>
-                        </div>
-                    </div>
-                </div>
-            </a>
-            <a href="detailpage.html" class="fadedownanimate" data-aos="fade-down" data-aos-delay="450" data-aos-duration="1000">
-                <div class="relative overflow-hidden">
-                    <img src="<?php echo trailingslashit( get_template_directory_uri() );?>assets/images/trekking/trek5.jpg" alt="Trekking Photo" class="transition ease-linear duration-150 hover:scale-110"/>                    
-                    <div class="absolute top-0 right-0">
-                        <div class="date flex bg-primary items-center p-1 md:p-2 lg:p-3 space-x-2 md:space-x-3">
-                            <span><i class="text-white text-xl md:text-2xl icon-CalendarDots"></i></span>
-                            <span class="text-white font-semibold text-base">April-June</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="trekdescription py-4">
-                    <h3 class="text-netrual-100 text-xl md:text-2xl font-bold mb-3 transition ease-linear delay-150 hover:text-primary">Kwangde waterfall Ice Climbing</h3>
-                    <div class="flex items-center space-x-6">
-                        <div class="flex items-center space-x-1">
-                            <span><i class="text-base md:text-lg text-neutral-800 icon-chart"></i></span>
-                            <span class="text-base md:text-lg text-neutral-800 font-semibold">Medium</span>
-                        </div>
-                        <div class="flex items-center space-x-1">
-                            <span><i class="text-base md:text-lg text-neutral-800 icon-clock"></i></span>
-                            <span class="text-base md:text-lg text-neutral-800 font-semibold">8 days</span>
-                        </div>
-                        <div class="flex items-center space-x-1">
-                            <span><i class="text-base md:text-lg text-neutral-800 icon-mountain"></i></span>
-                            <span class="text-base md:text-lg text-neutral-800 font-semibold">6,856m</span>
-                        </div>
-                    </div>
-                </div>
-            </a>
-            <a href="detailpage.html" class="fadedownanimate" data-aos="fade-down" data-aos-delay="550" data-aos-duration="1000">
-                <div class="relative overflow-hidden">
-                    <img src="<?php echo trailingslashit( get_template_directory_uri() );?>assets/images/trekking/trek6.jpg" alt="Trekking Photo" class="transition ease-linear duration-150 hover:scale-110"/>                    
-                </div>
-                <div class="trekdescription py-4">
-                    <h3 class="text-netrual-100 text-xl md:text-2xl font-bold mb-3 transition ease-linear delay-150 hover:text-primary">Khopra Trek via Ghorepani</h3>
-                    <div class="flex items-center space-x-6">
-                        <div class="flex items-center space-x-1">
-                            <span><i class="text-base md:text-lg text-neutral-800 icon-chart"></i></span>
-                            <span class="text-base md:text-lg text-neutral-800 font-semibold">Easy</span>
-                        </div>
-                        <div class="flex items-center space-x-1">
-                            <span><i class="text-base md:text-lg text-neutral-800 icon-clock"></i></span>
-                            <span class="text-base md:text-lg text-neutral-800 font-semibold">13 days</span>
-                        </div>
-                        <div class="flex items-center space-x-1">
-                            <span><i class="text-base md:text-lg text-neutral-800 icon-mountain"></i></span>
-                            <span class="text-base md:text-lg text-neutral-800 font-semibold">6,156m</span>
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-    </div>
-</section>
+    </section>
+    <?php
+}
