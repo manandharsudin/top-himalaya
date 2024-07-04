74d65a9ed9186e6981c80759f4fadeb5f1034546
sudinmanandhar <manandharsudin@gmail.com>
2024-07-04 09:28:41 +0545

    Completed blog page

--- index.php
+++ index.php
@@ -16,42 +16,41 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
+        <section class="ourBlogsWrapper pt-12 md:pt-16 pb-12 md:pb-16 lg:pb-34">
+            <div class="container">
+                <div class="blogwrapper grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8" data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000">
+                    <?php
+                    if ( have_posts() ) :
 
-		<?php
-		if ( have_posts() ) :
+                        /* Start the Loop */
+                        while ( have_posts() ) :
+                            the_post();
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
+                            /*
+                            * Include the Post-Type-specific template for the content.
+                            * If you want to override this in a child theme, then include a file
+                            * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+                            */
+                            get_template_part( 'template-parts/content', get_post_type() );
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+                        endwhile;
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+                        the_posts_pagination( array(
+                            'prev_text'          => '<span class="icon-arrow-left"></span>',
+                            'next_text'          => '<span class="icon-arrow-right"></span>',
+                            'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'top-himalaya' ) . ' </span>',
+                        ) );
 
-			endwhile;
+                    else :
 
-			the_posts_navigation();
+                        get_template_part( 'template-parts/content', 'none' );
 
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
+                    endif;
+                    ?>
+                </div>
+            </div>
+        </section>
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
--- template-parts/content.php
+++ template-parts/content.php
@@ -9,55 +9,9 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				top_himalaya_posted_on();
-				top_himalaya_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
-
-	<?php top_himalaya_post_thumbnail(); ?>
-
-	<div class="entry-content">
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'top-himalaya' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'top-himalaya' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php top_himalaya_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+<a href="<?php the_permalink(); ?>" class="bloglist block relative">
+    <?php the_post_thumbnail( 'full', array( 'class' => 'grayscale w-full h-80 object-cover' ) ); ?>
+    <div class="p-6 absolute left-0 right-0 bottom-0">
+        <?php the_title( '<h2 class="text-2xl font-semibold leading[120%] text-white">', '</h2>' ); ?>
+    </div>
+</a>
