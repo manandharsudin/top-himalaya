97647e6fa06076458c3268df02448e3dd2c03577
sudinmanandhar <manandharsudin@gmail.com>
2024-07-07 17:41:05 +0545

    Updated popular trip section of home page

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -165,99 +165,101 @@ function top_himalaya_search_form(){
 add_filter( 'get_search_form', 'top_himalaya_search_form' );
 
 function top_himalaya_related_trip(){ 
-    global $post;
-    $args = array(
-        'post_type'      => 'trip',
-        'post_status'    => 'publish',
-        'posts_per_page' => 3,
-        'post__not_in'   => array( $post->ID ),
-    );
+    if( is_singular( 'trip' ) ){
+        global $post;
+        $args = array(
+            'post_type'      => 'trip',
+            'post_status'    => 'publish',
+            'posts_per_page' => 3,
+            'post__not_in'   => array( $post->ID ),
+        );
 
-    $qry = new WP_Query( $args );
+        $qry = new WP_Query( $args );
 
-    if( $qry->have_posts() ){ ?>
-        <section class="popularTrek py-12 md:py-16 lg:py-20 bg-secondarylight">
-            <div class="container">
-                <div class="sectiontitle" data-aos="fade-down" data-aos-delay="50" data-aos-duration="1000">
-                    <h2 class="text-netrual-100 text-3xl md:text-4xl lg:text-[40px] lg:leading-[120%] font-bold mb-4 md:mb-8 lg:mb-14">Similar Trips</h2>
-                </div>
-                <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8" data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000">
-                    <?php
-                        while( $qry->have_posts() ){
-                            $qry->the_post();
-                            if( have_rows( 'trip_details' ) ){ 
-                                while( have_rows( 'trip_details' ) ) : the_row(); 
-                                    if( get_row_layout() == 'overview_section' ){ 
-                                        $text          = get_sub_field( 'text' );
-                                        $difficulty    = get_sub_field( 'difficulty' );
-                                        $duration      = get_sub_field( 'duration' );
-                                        $max_elevation = get_sub_field( 'max_elevation' );
-                                        $date          = get_sub_field( 'date' ); ?>
-                                        <a href="<?php the_permalink(); ?>">
-                                            <div class="relative overflow-hidden">
-                                                <?php 
-                                                    the_post_thumbnail( 'full', array( 'class' => 'transition ease-linear duration-150 hover:scale-110' ) );
+        if( $qry->have_posts() ){ ?>
+            <section class="popularTrek py-12 md:py-16 lg:py-20 bg-secondarylight">
+                <div class="container">
+                    <div class="sectiontitle" data-aos="fade-down" data-aos-delay="50" data-aos-duration="1000">
+                        <h2 class="text-netrual-100 text-3xl md:text-4xl lg:text-[40px] lg:leading-[120%] font-bold mb-4 md:mb-8 lg:mb-14">Similar Trips</h2>
+                    </div>
+                    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8" data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000">
+                        <?php
+                            while( $qry->have_posts() ){
+                                $qry->the_post();
+                                if( have_rows( 'trip_details' ) ){ 
+                                    while( have_rows( 'trip_details' ) ) : the_row(); 
+                                        if( get_row_layout() == 'overview_section' ){ 
+                                            $text          = get_sub_field( 'text' );
+                                            $difficulty    = get_sub_field( 'difficulty' );
+                                            $duration      = get_sub_field( 'duration' );
+                                            $max_elevation = get_sub_field( 'max_elevation' );
+                                            $date          = get_sub_field( 'date' ); ?>
+                                            <a href="<?php the_permalink(); ?>">
+                                                <div class="relative overflow-hidden">
+                                                    <?php 
+                                                        the_post_thumbnail( 'full', array( 'class' => 'transition ease-linear duration-150 hover:scale-110' ) );
 
-                                                    if( $date ){ ?>                                    
-                                                        <div class="absolute top-0 right-0">
-                                                            <div class="date flex bg-primary items-center p-1 md:p-2 lg:p-3 space-x-2 md:space-x-3">
-                                                                <span><i class="text-white text-xl md:text-2xl icon-CalendarDots"></i></span>
-                                                                <span class="text-white font-semibold text-base"><?php echo esc_html( $date ); ?></span>
+                                                        if( $date ){ ?>                                    
+                                                            <div class="absolute top-0 right-0">
+                                                                <div class="date flex bg-primary items-center p-1 md:p-2 lg:p-3 space-x-2 md:space-x-3">
+                                                                    <span><i class="text-white text-xl md:text-2xl icon-CalendarDots"></i></span>
+                                                                    <span class="text-white font-semibold text-base"><?php echo esc_html( $date ); ?></span>
+                                                                </div>
                                                             </div>
-                                                        </div>
-                                                        <?php
-                                                    }
-                                                ?>
-                                            </div>
-                                            <div class="trekdescription py-4">
-                                                <h3 class="text-netrual-100 text-xl md:text-2xl font-bold mb-3 transition ease-linear delay-150 hover:text-primary"><?php the_title(); ?></h3>
-                                                <p class="text-ellipse line-clamp-2 overflow-hidden text-neutral-800 mb-4"><?php echo wp_trim_words( $text, 30 ); ?></p>
-                                                <?php
-                                                    if( $difficulty || $duration || $max_elevation ){ ?>                                        
-                                                        <div class="flex items-center space-x-6">
-                                                            <?php 
-                                                                if( $difficulty ){ ?>
-                                                                    <div class="flex items-center space-x-1">
-                                                                        <span><i class="text-base md:text-lg text-neutral-800 icon-chart"></i></span>
-                                                                        <span class="text-base md:text-lg text-neutral-800 font-semibold"><?php echo esc_html( $difficulty ); ?></span>
-                                                                    </div>
-                                                                    <?php
-                                                                }
-        
-                                                                if( $duration ){ ?>
-                                                                    <div class="flex items-center space-x-1">
-                                                                        <span><i class="text-base md:text-lg text-neutral-800 icon-clock"></i></span>
-                                                                        <span class="text-base md:text-lg text-neutral-800 font-semibold"><?php echo esc_html( $duration ); ?></span>
-                                                                    </div>
-                                                                    <?php
-                                                                }
-        
-                                                                if( $max_elevation ){ ?>
-                                                                    <div class="flex items-center space-x-1">
-                                                                        <span><i class="text-base md:text-lg text-neutral-800 icon-mountain"></i></span>
-                                                                        <span class="text-base md:text-lg text-neutral-800 font-semibold"><?php echo esc_html( $max_elevation ); ?></span>
-                                                                    </div>
-                                                                    <?php
-                                                                }
-                                                            ?>
-                                                        </div>
-                                                        <?php
-                                                    }
-                                                ?>
-                                            </div>
-                                        </a>
-                                        <?php
-                                    }
-                                endwhile;
-                            }
-                        }        
-                    ?>                    
+                                                            <?php
+                                                        }
+                                                    ?>
+                                                </div>
+                                                <div class="trekdescription py-4">
+                                                    <h3 class="text-netrual-100 text-xl md:text-2xl font-bold mb-3 transition ease-linear delay-150 hover:text-primary"><?php the_title(); ?></h3>
+                                                    <p class="text-ellipse line-clamp-2 overflow-hidden text-neutral-800 mb-4"><?php echo wp_trim_words( $text, 30 ); ?></p>
+                                                    <?php
+                                                        if( $difficulty || $duration || $max_elevation ){ ?>                                        
+                                                            <div class="flex items-center space-x-6">
+                                                                <?php 
+                                                                    if( $difficulty ){ ?>
+                                                                        <div class="flex items-center space-x-1">
+                                                                            <span><i class="text-base md:text-lg text-neutral-800 icon-chart"></i></span>
+                                                                            <span class="text-base md:text-lg text-neutral-800 font-semibold"><?php echo esc_html( $difficulty ); ?></span>
+                                                                        </div>
+                                                                        <?php
+                                                                    }
+            
+                                                                    if( $duration ){ ?>
+                                                                        <div class="flex items-center space-x-1">
+                                                                            <span><i class="text-base md:text-lg text-neutral-800 icon-clock"></i></span>
+                                                                            <span class="text-base md:text-lg text-neutral-800 font-semibold"><?php echo esc_html( $duration ); ?></span>
+                                                                        </div>
+                                                                        <?php
+                                                                    }
+            
+                                                                    if( $max_elevation ){ ?>
+                                                                        <div class="flex items-center space-x-1">
+                                                                            <span><i class="text-base md:text-lg text-neutral-800 icon-mountain"></i></span>
+                                                                            <span class="text-base md:text-lg text-neutral-800 font-semibold"><?php echo esc_html( $max_elevation ); ?></span>
+                                                                        </div>
+                                                                        <?php
+                                                                    }
+                                                                ?>
+                                                            </div>
+                                                            <?php
+                                                        }
+                                                    ?>
+                                                </div>
+                                            </a>
+                                            <?php
+                                        }
+                                    endwhile;
+                                }
+                            }        
+                        ?>                    
+                    </div>
                 </div>
-            </div>
-        </section>    
-        <?php
+            </section>    
+            <?php
+        }
+        wp_reset_postdata();
     }
-    wp_reset_postdata();
 }
 add_action( 'top_himalaya_before_footer', 'top_himalaya_related_trip' );
 
--- template-parts/home/popular.php
+++ template-parts/home/popular.php
@@ -6,7 +6,17 @@
 $title = get_sub_field( 'title' );
 $trips = get_sub_field( 'trips' );
 
-if( $trips ){ ?>
+$args = array(
+    'post_type'      => 'trip',
+    'post_status'    => 'publish',
+    'posts_per_page' => -1,
+    'post__in'       => $trips,
+    'orderby'        => 'post__in'
+);
+
+$qry = new WP_Query( $args );
+
+if( $trips && $qry->have_posts() ){ ?>
     <section class="popularTrek py-12 md:py-16 lg:py-20">
         <div class="container">
             <?php
@@ -21,78 +31,72 @@ if( $trips ){ ?>
             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
                 <?php
                     $i = 50;
-                    foreach( $trips as $trip ){ 
-                        $image      = $trip['image'];
-                        $date       = $trip['date'];
-                        $title      = $trip['title'];
-                        $difficulty = $trip['difficulty'];
-                        $days       = $trip['days'];
-                        $height     = $trip['height'];
-                        $link       = $trip['link']; ?>
-                        <a href="<?php echo esc_url( $link['url'] )?>" target="<?php echo esc_attr( $link['target'] ); ?>" class="fadedownanimate" data-aos="fade-down" data-aos-delay="<?php echo absint( $i ); ?>" data-aos-duration="1000">
-                            <?php
-                                if( $image ){ ?>
-                                    <div class="relative overflow-hidden">
-                                        <?php 
-                                            echo wp_get_attachment_image( $image['ID'], 'full', false, array( 'class' => 'transition ease-linear duration-150 hover:scale-110' ) );
-                                            
-                                            if( $date ){ ?>
-                                                <div class="absolute top-0 right-0">
-                                                    <div class="date flex bg-primary items-center p-1 md:p-2 lg:p-3 space-x-2 md:space-x-3">
-                                                        <span><i class="text-white text-xl md:text-2xl icon-CalendarDots"></i></span>
-                                                        <span class="text-white font-semibold text-base"><?php echo esc_html( $date ); ?></span>
+                    while( $qry->have_posts() ){
+                        $qry->the_post();
+                        if( have_rows( 'trip_details' ) ){ 
+                            while( have_rows( 'trip_details' ) ) : the_row(); 
+                                if( get_row_layout() == 'overview_section' ){ 
+                                    $difficulty    = get_sub_field( 'difficulty' );
+                                    $duration      = get_sub_field( 'duration' );
+                                    $max_elevation = get_sub_field( 'max_elevation' );
+                                    $date          = get_sub_field( 'date' ); ?>
+                                    <a href="<?php the_permalink(); ?>" class="fadedownanimate" data-aos="fade-down" data-aos-delay="<?php echo absint( $i ); ?>" data-aos-duration="1000">
+                                        <div class="relative overflow-hidden">
+                                            <?php 
+                                                the_post_thumbnail( 'full', array( 'class' => 'transition ease-linear duration-150 hover:scale-110' ) );
+
+                                                if( $date ){ ?>                                    
+                                                    <div class="absolute top-0 right-0">
+                                                        <div class="date flex bg-primary items-center p-1 md:p-2 lg:p-3 space-x-2 md:space-x-3">
+                                                            <span><i class="text-white text-xl md:text-2xl icon-CalendarDots"></i></span>
+                                                            <span class="text-white font-semibold text-base"><?php echo esc_html( $date ); ?></span>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <?php
-                                            }
-                                        ?>
-                                    </div>
-                                    <?php      
-                                }
-
-                                if( $title || $difficulty || $days || $height ){ ?>                                
-                                    <div class="trekdescription py-4">
-                                        <?php
-                                            if( $title ) echo '<h3 class="text-netrual-100 text-xl md:text-2xl font-bold mb-3 transition ease-linear delay-150 hover:text-primary">' . esc_html( $title ) . '</h3>';
-
-                                            if( $difficulty || $days || $height ){ ?>                                        
-                                                <div class="flex items-center space-x-6">
-                                                    <?php 
-                                                        if( $difficulty ){ ?>
-                                                            <div class="flex items-center space-x-1">
-                                                                <span><i class="text-base md:text-lg text-neutral-800 icon-chart"></i></span>
-                                                                <span class="text-base md:text-lg text-neutral-800 font-semibold"><?php echo esc_html( $difficulty ); ?></span>
-                                                            </div>
-                                                            <?php
-                                                        }
-
-                                                        if( $days ){ ?>
-                                                            <div class="flex items-center space-x-1">
-                                                                <span><i class="text-base md:text-lg text-neutral-800 icon-clock"></i></span>
-                                                                <span class="text-base md:text-lg text-neutral-800 font-semibold"><?php echo esc_html( $days ); ?></span>
-                                                            </div>
-                                                            <?php
-                                                        }
-
-                                                        if( $height ){ ?>
-                                                            <div class="flex items-center space-x-1">
-                                                                <span><i class="text-base md:text-lg text-neutral-800 icon-mountain"></i></span>
-                                                                <span class="text-base md:text-lg text-neutral-800 font-semibold"><?php echo esc_html( $height ); ?></span>
-                                                            </div>
-                                                            <?php
-                                                        }
-                                                    ?>
-                                                </div>
-                                                <?php
-                                            }
-                                        ?>
-                                    </div>
+                                                    <?php
+                                                }
+                                            ?>
+                                        </div>
+                                        <div class="trekdescription py-4">
+                                            <h3 class="text-netrual-100 text-xl md:text-2xl font-bold mb-3 transition ease-linear delay-150 hover:text-primary"><?php the_title(); ?></h3>                                            
+                                            <?php
+                                                if( $difficulty || $duration || $max_elevation ){ ?>                                        
+                                                    <div class="flex items-center space-x-6">
+                                                        <?php 
+                                                            if( $difficulty ){ ?>
+                                                                <div class="flex items-center space-x-1">
+                                                                    <span><i class="text-base md:text-lg text-neutral-800 icon-chart"></i></span>
+                                                                    <span class="text-base md:text-lg text-neutral-800 font-semibold"><?php echo esc_html( $difficulty ); ?></span>
+                                                                </div>
+                                                                <?php
+                                                            }
+    
+                                                            if( $duration ){ ?>
+                                                                <div class="flex items-center space-x-1">
+                                                                    <span><i class="text-base md:text-lg text-neutral-800 icon-clock"></i></span>
+                                                                    <span class="text-base md:text-lg text-neutral-800 font-semibold"><?php echo esc_html( $duration ); ?></span>
+                                                                </div>
+                                                                <?php
+                                                            }
+    
+                                                            if( $max_elevation ){ ?>
+                                                                <div class="flex items-center space-x-1">
+                                                                    <span><i class="text-base md:text-lg text-neutral-800 icon-mountain"></i></span>
+                                                                    <span class="text-base md:text-lg text-neutral-800 font-semibold"><?php echo esc_html( $max_elevation ); ?></span>
+                                                                </div>
+                                                                <?php
+                                                            }
+                                                        ?>
+                                                    </div>
+                                                    <?php
+                                                }
+                                            ?>
+                                        </div>
+                                    </a>
                                     <?php
                                 }
-                            ?>
-                        </a>
-                        <?php
-                        $i = $i + 100;
+                            endwhile;
+                        }
+                        $i = $i + 100;                    
                     }
                 ?>
             </div>
@@ -100,3 +104,4 @@ if( $trips ){ ?>
     </section>
     <?php
 }
+wp_reset_postdata();
